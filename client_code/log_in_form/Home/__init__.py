bed8f0298b037bb205f8c175b361f0ce43441f39
sowmyas1117@gmail.com <sowmyas1117@gmail.com>
2023-11-02 08:29:07 +0000

    Edited log_in_form.Home and theme/parameters.yaml

--- client_code/log_in_form/Home/__init__.py
+++ client_code/log_in_form/Home/__init__.py
@@ -16,53 +16,7 @@ class Home(HomeTemplate):
     """This method is called when the button is clicked"""
     open_form('log_in_form')
 
-  def link_1_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('log_in_form.Home.admin_teams')
 
-  def link_2_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('log_in_form.Home.borrowers') 
-
-  def link_3_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('log_in_form.Home.lenders')
-
-  def link_4_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('log_in_form.Home.loan_management')
-
-  def link_5_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('log_in_form.Home.manage_products')
-
-  def link_6_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('log_in_form.Home.manage_settings')
-    
-  def link_7_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('log_in_form.Home.revenue_share')
-
-  def link_8_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('log_in_form.Home.risk_pool')
-
-  def link_9_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('log_in_form.Home.manage_cms')
-
-  def link_10_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('log_in_form.Home.manage_reports')
-
-  def link_11_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('log_in_form.Home.ROI')
-
-  def link_12_click(self, **event_args):
-    """This method is called when the link is clicked"""
-    open_form('log_in_form.Home.performance_tracke')
 
     
     
